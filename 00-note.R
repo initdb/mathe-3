e02f232ba32ab194754e28d977e5bf2d69a92430
initdb <benedict.schwind@live.de>
2019-03-19 09:09:41 +0100

    initial commit of R scripts

---
+++ 00-note.R
@@ -0,0 +1,5 @@
+#install.package("HistogramTools")
+#library(HistogramTools)
+
+# zuweisen mit <-
+x <- c(1.97,2.33)
---
+++ 01-einfuehrung-in-R/04-data-frames.R
@@ -0,0 +1,12 @@
+# this is a comment
+
+# load mtcars data
+data("mtcars")
+
+# keep the data loaded
+attach(mtcars)
+
+# draw x/y-plot
+plot(mpg,wt)
+
+cor(mpg,wt)
